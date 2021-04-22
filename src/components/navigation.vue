3475905fe9daab2384877ae4fedff48df8332097
oscar666666 <oscar88feichang6@gmail.com>
2021-04-21 19:48:15 -0600

    implement resiponsive design to navbar

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <b-navbar class="fixed-top" type="dark" variant="dark">
+    <b-navbar class="fixed-top" toggleable="lg" type="dark" variant="dark">
       <b-navbar-brand href="/">
         <img src="../assets/home-5-fill.png" class="d-inline-block align-top" />
         Home
       </b-navbar-brand>
-      <!-- Left aligned nav items -->
+    <!-- collapse button responsive design -->
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+<!-- Left aligned nav items -->
       <b-navbar-nav>
-        <a href="/files/Oscar-Lou-Resume-2021.pdf" target="_blank"
+        <a href="https://github.com/oscar666666/vue-portfolio-oscar/blob/main/public/files/Oscar-Lou-Resume-2021.pdf" target="_blank"
           ><b-button variant="outline-light">Resume </b-button></a
         >
         <!--<b-nav-item href="#about">About</b-nav-item>-->
@@ -22,6 +26,7 @@
 
         <b-nav-item href="#skills">3.Skills</b-nav-item>
       </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
